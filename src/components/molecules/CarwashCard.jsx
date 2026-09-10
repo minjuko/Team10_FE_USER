cc23ed22b535628b923d35f10a04b9838b97995d
minjuko <rhalswn0526@gmail.com>
2026-09-11 00:34:41 +0900

    fix: 추천 카드 이미지 비율 보존

--- src/components/molecules/CarwashCard.jsx
+++ src/components/molecules/CarwashCard.jsx
@@ -26,11 +26,11 @@ export const CarwashCard = ({ id, image, name, address, rate, distance }) => {
       role="button"
       tabIndex={0}
     >
-      <div className="relative h-36">
+      <div className="relative h-36 overflow-hidden bg-slate-100">
         <img
           src={imageUrl}
           alt={name}
-          className="absolute object-cover -translate-y-1/2 top-1/2"
+          className="absolute inset-0 w-full h-full object-contain"
         />
       </div>
       <div className="absolute bottom-0 z-10 w-full h-24 p-4 bg-white">
